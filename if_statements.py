8f3dad261fbb2ec17545a64c88a5742f286b9b75
joudaPROTO <jouda.asaad98@gmail.com>
2021-02-14 16:47:20 +0400

    changes in if-else

--- if_statements.py
+++ if_statements.py
@@ -1,10 +1,13 @@
-age = 0
+age = 50
 
 if age < 4:
     price = 0
 elif age < 18:
     price = 25
+elif age < 65:
+    price = 69
 else:
     price = 48
 
 print(f"your admission cost is {price} AED.")
+
